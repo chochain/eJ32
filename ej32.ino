59ff46d6fe435d126dbd7947f60e651716b4e3ae
gnii <chochain@yahoo.com>
2022-01-09 17:49:19 +0800

    ~/ej32.ino update to nanojvm 1.0

--- ej32.ino
+++ ej32.ino
@@ -2,16 +2,24 @@
 /// eJ32
 ///
 #include "src/mmu.h"
-#include "src/jvm.h"
-
+#include "src/forth.h"
+#include "src/java.h"
+///
+/// console interface
+///
+void send_to_console(int, const char* msg) { Serial.print(msg); }
+///
+/// Arduino core
+///
 void setup() {
     Serial.begin(115200);
     delay(100);
-    
-    int err = jvm_setup("/java/ESP32Test.class");
-    if (err) { LOG("jvm_setup failed: "); LOX(err); }
+
+    forth_setup(send_to_console);
+    int err = java_setup("/java/ESP32Test.class", send_to_console);
+    if (err) { LOG("java_setup failed: "); LOX(err); }
 }
 
 void loop() {
-    jvm_run();
+    java_run();
 }
